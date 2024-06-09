54b44ee55561aa92a25e4c817cda46a6c52c4132
EgyptaGogoi <egypta31@gmail.com>
2024-06-10 00:44:34 +0530

    Pleasee my changee!

---
+++ month1/week1/hackerrank.java
@@ -0,0 +1,15 @@
+public class hackerrank{
+
+    // Introduction (10/06/24)
+    
+    // public static void main(String arg[])
+    // {
+    //     System.out.println("Hello, World.");
+    //     System.out.println("Hello, Java.");
+    // }
+
+    /**************************************************************************************** */
+
+
+
+}
