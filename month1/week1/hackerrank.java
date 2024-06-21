32ded08ed49274a7b29ca9864d88c992f8b1812c
Nayanmoni <nayancoding@gmial.com>
2024-06-21 21:13:32 +0530

    solved hackerrank codes

--- month1/week1/hackerrank.java
+++ month1/week1/hackerrank.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;;
+
 public class hackerrank{
 
     // Introduction (10/06/24)
@@ -10,6 +14,66 @@ public class hackerrank{
 
     /**************************************************************************************** */
 
+    // public static void main(String []argh)
+    // {
 
 
+
+    //     Scanner sc = new Scanner(System.in);
+    //     int t=sc.nextInt();
+
+    //     for(int i=0;i<t;i++)
+    //     {
+
+    //         try
+    //         {
+    //             long x=sc.nextLong();
+    //             System.out.println(x+" can be fitted in:");
+                
+    //             if(x>= Byte.MIN_VALUE && x<= Byte.MAX_VALUE )
+    //             System.out.println("* byte");
+    //             if(x >= Short.MIN_VALUE && x <= Short.MAX_VALUE)
+    //             System.out.println("* short");
+    //             if(x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE)
+    //             System.out.println("* int");
+    //             if(x >= Long.MIN_VALUE && x <= Long.MAX_VALUE)
+    //             System.out.println("* long");
+    //         }
+    //         catch(Exception e)
+    //         {
+    //             System.out.println(sc.next()+" can't be fitted anywhere.");
+    //         }
+
+    //     }
+    // }
+
+    /**************************************************************************************** */
+
+    // public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //     System.out.println("================================");
+    //     for(int i=0;i<3;i++){
+    //         String s1=sc.next();
+    //         int x= sc.nextInt() ;
+    //         //Complete this line
+            
+    //         System.out.printf("%-15s%03d\n",s1, x);
+            
+            
+    //     }
+    //     System.out.println("================================");
+
+    // }
+
+    /**************************************************************************************** */
+
+    
+    /**************************************************************************************** */
+    /**************************************************************************************** */
+    /**************************************************************************************** */
+    /**************************************************************************************** */
+    /**************************************************************************************** */
+    /**************************************************************************************** */
+    /**************************************************************************************** */
+    /**************************************************************************************** */
 }
--- month1/week1/index.java
+++ month1/week1/index.java
@@ -1,6 +1,50 @@
+import java.io.*;
+
 public class index { 
+    // public static void main(String args[]) 
+    // { 
+    //     System.out.println("Hello World"); 
+    // } 
+
+    /******************************************************************************************************************************************** */
+
+    // Output ( System.out )
+
+        // public static void main(String args[]){
+
+        //     String firstName = "Egypta";
+        //     String lastName = "Gogoi";
+        //     int age = 23;
+            
+        //     // print()
+        //     //  simple print function
+        //     // The cursor remains at the end of the text on the console.
+        //     System.out.print("\nprint(): The cursor remains at the end of the text on the console.\n\n");
+
+        //     // println()
+        //     // the cursor is at the starting of the next line.
+        //     System.out.println("println(): I print a line and the cursor is at the starting of the next line.\n");
+
+        //     // printf
+        //     // similar to printf in C
+        //     // String concatination and formate specifiers
+        //     System.out.printf("printf(): similar to printf in C.\n");
+        //     System.out.printf("String concatination and formate specifier:\n" + firstName + " " + lastName + " ,her age is %d\n",age);
+        // }
+
+    // Error ( System.in )
+    
     public static void main(String args[]) 
+    throws IOException 
     { 
-        System.out.println("Hello World"); 
+        
     } 
+
+
+    /*
+        public static void main(String args[]){
+            byte a = 127;
+            System.out.println((int)a);
+        }
+     */
 }
